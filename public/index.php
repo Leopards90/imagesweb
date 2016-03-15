be25e21c4e748e80d702531e5470a18df2069d78
Leopards <mainghoangtam@gmail.com>
2016-03-15 17:54:14 +0700

    config multiple

--- public/index.php
+++ public/index.php
@@ -1,34 +1,25 @@
 <?php
 
-error_reporting(E_ALL);
+    error_reporting(E_ALL);
 
-define('APP_PATH', realpath('..'));
+    try {
 
-try {
+        /**
+         * Read the configuration
+         */
+        $config = include __DIR__ . "/../apps/config/config.php";
 
-    /**
-     * Read the configuration
-     */
-    $config = include APP_PATH . "/app/config/config.php";
+        /**
+         * Read auto-loader in Moudel in backeend vs forntend
+         */
+        //include __DIR__ . "/../apps/econfig/loader.php";
 
-    /**
-     * Read auto-loader
-     */
-    include APP_PATH . "/app/config/loader.php";
+        /**
+         * Read services
+         */
+        include __DIR__ . "/../apps/config/services.php";
 
-    /**
-     * Read services
-     */
-    include APP_PATH . "/app/config/services.php";
-
-    /**
-     * Handle the request
-     */
-    $application = new \Phalcon\Mvc\Application($di);
-
-    echo $application->handle()->getContent();
-
-} catch (\Exception $e) {
-    echo $e->getMessage() . '<br>';
-    echo '<pre>' . $e->getTraceAsString() . '</pre>';
-}
+    } catch (Exception $e) {
+        echo $e->getMessage(), '<br>';
+        echo nl2br(htmlentities($e->getTraceAsString()));
+    }
